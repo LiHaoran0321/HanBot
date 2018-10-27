8f7ef533e861ad97256fed5c8c4c82b85c83ab9d
HealerBot <wontbebanned001@gmail.com>
2018-10-28 00:41:35 +0300

    Version 1.0

---
+++ Healer AIO/header.lua
@@ -0,0 +1,28 @@
+local HealerAIOchampions = {
+  Yorick = true,
+  Sona = true,
+  Ivern = true,
+  Kayle = true,
+  Karthus = true,
+  Kennen = true,
+  Kindred = true,
+  Lux = true,
+  Malzahar = true
+}
+
+return {
+  id = "HealerAIO" .. player.charName,
+  name = "Healer AIO - " .. player.charName,
+  riot = true,
+  flag = {
+    text = "H E A L E R   A I O",
+    color = {
+      text = 0xffeeeeee,
+      background1 = 0xffeeeeee,
+      background2 = 0xFF21214E
+    }
+  },
+  load = function()
+    return HealerAIOchampions[player.charName]
+  end
+}
